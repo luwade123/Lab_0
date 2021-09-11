4d4551647b2d1c8fb4f9d9db63a8527ac275a294
luwade123 <32285236+luwade123@users.noreply.github.com>
2021-09-11 21:20:06 +0800

    Create lab0.py

---
+++ lab0.py
@@ -0,0 +1,17 @@
+def myFunction():
+    '''
+    Write a python function that return the sum of multiples of 3 or 5 between 0 and 100
+    And print the answer before you return
+    You can write in python2 or python3 :D
+    '''
+
+
+
+
+
+
+    return sum
+
+
+if __name__ == '__main__':
+    myFunction()
